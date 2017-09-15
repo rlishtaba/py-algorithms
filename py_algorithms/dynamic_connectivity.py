2516ac1e729b09d0ce38fd8bd61dddb21e5ac0d5
Roman Lishtaba <roman@lishtaba.com>
2017-09-15 00:01:09 -0400

    Adding quick union algo

--- py_algorithms/dynamic_connectivity.py
+++ py_algorithms/dynamic_connectivity.py
@@ -6,13 +6,17 @@ def dynamic_connectivity_brute_force(components: List[int]) -> 'DynamicConnectiv
     return _BruteForce(components)
 
 
+def dynamic_connectivity_quick_union(components: List[int]) -> 'DynamicConnectivity':
+    return _QuickUnion(components)
+
+
 class DynamicConnectivity(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def is_connected(self, a: int, b: int) -> bool:
         pass
 
     @abc.abstractmethod
-    def connect(self, a: int, b: int) -> bool:
+    def connect(self, a: int, b: int) -> None:
         pass
 
 
@@ -23,13 +27,31 @@ class _BruteForce(DynamicConnectivity):
     def is_connected(self, a: int, b: int) -> bool:
         return self._components[a] == self._components[b]
 
-    def connect(self, a: int, b: int) -> bool:
+    def connect(self, a: int, b: int) -> None:
         a_ptr = self._components[a]
         b_ptr = self._components[b]
         # test if already connected
         if a_ptr == b_ptr:
-            return True
+            return
 
         for i, x in enumerate(self._components):
             if self._components[i] == a_ptr:
                 self._components[i] = b_ptr
+
+
+class _QuickUnion(DynamicConnectivity):
+    def __init__(self, components: List[int]):
+        self._components = components
+
+    def is_connected(self, a: int, b: int) -> bool:
+        return self._root(a) == self._root(b)
+
+    def connect(self, a: int, b: int) -> None:
+        a_ptr = self._root(a)
+        b_ptr = self._root(b)
+        self._components[a_ptr] = b_ptr
+
+    def _root(self, x: int) -> int:
+        while x != self._components[x]:
+            x = self._components[x]
+        return x
--- tests/py_algorithms/dynamic_connectivity_test.py
+++ tests/py_algorithms/dynamic_connectivity_test.py
@@ -1,32 +1,43 @@
+from typing import Callable
+from typing import List
+
 import pytest
 
 from py_algorithms.dynamic_connectivity import dynamic_connectivity_brute_force
+from py_algorithms.dynamic_connectivity import dynamic_connectivity_quick_union
+from py_algorithms.dynamic_connectivity import DynamicConnectivity
 
 
 @pytest.fixture
-def payload():
+def payload() -> List[int]:
     return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def helper_impl(f: Callable[[List[int]], DynamicConnectivity]) -> None:
+    impl = f(payload())
+    assert impl.is_connected(0, 9) is False
+    impl.connect(0, 9)
+    assert impl.is_connected(0, 9) is True
+
+    assert impl.is_connected(0, 1) is False
+    impl.connect(0, 1)
+    assert impl.is_connected(0, 9) is True
+
+    assert impl.is_connected(5, 8) is False
+    impl.connect(5, 8)
+    assert impl.is_connected(5, 8) is True
+
+    assert impl.is_connected(9, 8) is False
+    impl.connect(9, 8)
+    assert impl.is_connected(9, 8) is True
+
+    # Transitivity rule
+    assert impl.is_connected(5, 9) is True
 
 
 class TestDynamicConnectivity:
     def testBruteForce(self):
-        impl = dynamic_connectivity_brute_force(payload())
+        helper_impl(dynamic_connectivity_brute_force)
 
-        assert impl.is_connected(0, 9) is False
-        impl.connect(0, 9)
-        assert impl.is_connected(0, 9) is True
-
-        assert impl.is_connected(0, 1) is False
-        impl.connect(0, 1)
-        assert impl.is_connected(0, 9) is True
-
-        assert impl.is_connected(5, 8) is False
-        impl.connect(5, 8)
-        assert impl.is_connected(5, 8) is True
-
-        assert impl.is_connected(9, 8) is False
-        impl.connect(9, 8)
-        assert impl.is_connected(9, 8) is True
-
-        # Transitivity rule
-        assert impl.is_connected(5, 9) is True
+    def testQuickUnion(self):
+        helper_impl(dynamic_connectivity_quick_union)
